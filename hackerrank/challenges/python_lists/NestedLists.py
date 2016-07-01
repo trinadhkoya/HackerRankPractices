de8ee3dcbb6261f260448de6c5a0a79136d45eef
trinadhkoya <trinadhkoya9@gmail.com>
2016-07-01 14:23:02 +0530

    Hacker Rank Practices

--- hackerrank/challenges/python_lists/NestedLists.py
+++ hackerrank/challenges/python_lists/NestedLists.py
@@ -1,19 +1,14 @@
-#empty dictionary
-d = {}
-#looping till the given int satisfied
-for _ in range(int(input())):  # 2
-    Name = input()  # 3
-    Grade = float(input())  # 4
-    #d['name']=5.0
-    d[Name] = Grade  # 5
-#listing out values in dictionary
-v = d.values()  # 6
-#eleminating dupliactaes and make it listed .with sorting any way we could
-second = sorted(list(set(v)))[1]
+student_dic = {}
+for _ in range(int(input())):
+    Name = input()
+    Grade = float(input())
+    student_dic[Name] = Grade
+
+second = sorted(list(set(student_dic.values())))[1]
 second_lowest = []  # 8
-for key, value in d.items():
-    if value == second:
-        second_lowest.append(key)
+for stud_name, stud_mark in student_dic.items():
+    if stud_mark == second:
+        second_lowest.append(stud_name)
 second_lowest.sort()  # 12
 for name in second_lowest:
     print(name)
