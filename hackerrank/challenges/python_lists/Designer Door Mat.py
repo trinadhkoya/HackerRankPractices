762ca52d26397ec8998ef76e260e39e7e104148c
trinadhkoya <trinadhkoya9@gmail.com>
2016-07-04 15:12:52 +0530

    9 27 ------------.|.------------ ---------.|..|..|.--------- ------.|..|..|..|..|.------ ---.|..|..|..|..|..|..|.--- ----------WELCOME---------- ---.|..|..|..|..|..|..|.--- ------.|..|..|..|..|.------ ---------.|..|..|.--------- ------------.|.------------

--- hackerrank/challenges/python_lists/Designer Door Mat.py
+++ hackerrank/challenges/python_lists/Designer Door Mat.py
@@ -1,12 +1,13 @@
+# N, M = map(int,input().split()) # More than 6 lines of code will result in 0 score. Blank lines are not counted.
+# for i in range(1,N,2):
+#     print((i*".|.").center(M,'-'))  # Enter Code Here
+# print ("TRINADHK".center(M,'-'))#Enter Code Here
+# for i in range(N-2,-1,-2):
+#     print((i*".|.").center(M,'-'))  # Enter Code Here
+
 Height, Width = map(int, input().split())
 for i in range(1, Height, 2):
     print((i * ".|.").center(Width, '-'));
 print("WELCOME".center(Width, '-'));
 for i in range(Height-2, -1, -2):
     print((i*".|.").center(Width, '-'))
-
-
-# ## from discussions
-# n, m = map(int,input().split())
-# pattern = [('.|.'*(2*i + 1)).center(m, '-') for i in range(n//2)]
-# print('\n'.join(pattern + ['WELCOME'.center(m, '-')] + pattern[::-1]))
